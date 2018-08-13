de6d571db68d5c602d6895d1ea2e72bbdd278af4
Laris <laris.popa@yahoo.ro>
2018-08-13 17:44:41 +0300

    login&register full functionality

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var http = require('http');
 var mysql = require('mysql');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,7 +20,7 @@ const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "skyx"
+    database: "mydb"
 });
 
 app.get('/',function(req, res){
@@ -38,23 +42,14 @@ app.post('/register',function(req, res){
     var email = req.body.email;
     var password = req.body.password;
 
-    var post = {
-        name : name,
-        email : email,
-        password : password
-    };
-
-
-   //console.log("DATA: "+post);
-
-   con.query("INSERT INTO user SET ?",post,function(err,result){
+    con.query("INSERT INTO user SET name = ?, email = ?, password = ?",[name,email,password,email],function(err, result){
 
        if(err)
-           throw err
-       else
-           console.log(result);
-   });
-   res.redirect('/');
+           res.send("User already exist");
+       else{
+           res.redirect('/');
+       }
+    });
 });
 
 app.get('/login',function(req, res){
@@ -64,19 +59,41 @@ app.get('/login',function(req, res){
 
 app.post('/login',function(req,res){
 
-    var em = req.body.email;
-    var pass = req.body.password;
-
-
-   con.query("SELECT * FROM user WHERE email=? AND password=?",[em,pass],function(err,result){
-
-       if(err)
-           throw err;
-       else{
-           console.log(result);
-           res.redirect('/');
-       }
-   })
+    var email= req.body.email;
+    var password = req.body.password;
+    con.query('SELECT * FROM user WHERE email = ?',[email], function (error, results, fields) {
+        if (error) {
+            // console.log("error ocurred",error);
+            res.send({
+                "code":400,
+                "failed":"error ocurred"
+            })
+        }else{
+            // console.log('The solution is: ', results);
+            if(results.length >0){
+                if(results[0].password == password){
+                    res.cookie("emaill",results[0].email);
+                    res.cookie("passwordd",results[0].password);
+                    res.send({
+                        "code":200,
+                        "success":"login sucessfull"
+                    });
+                }
+                else{
+                    res.send({
+                        "code":204,
+                        "success":"Email or password is wrong!"
+                    });
+                }
+            }
+            else{
+                res.send({
+                    "code":204,
+                    "success":"Email does not exits"
+                });
+            }
+        }
+    });
 });
 
 
